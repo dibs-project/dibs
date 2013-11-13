1bb004f591eb60411b5bf604e5b0a65b28df3e3d
Sven Pfaller <sven.pfaller@hu-berlin.de>
2013-11-13 17:22:08 +0100

    Applicant.getApplicant implementiert (inklusive Tests)

--- src/test/java/de/huberlin/cms/hub/ApplicationServiceTest.java
+++ src/test/java/de/huberlin/cms/hub/ApplicationServiceTest.java
@@ -4,6 +4,7 @@
 
 package de.huberlin.cms.hub;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assume.assumeTrue;
 
@@ -17,10 +18,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ApplicationServiceTest {
+    ApplicationService service;
     Properties config;
 
     @Before
-    public void before() throws IOException {
+    public void before() throws IOException, SQLException {
         config = new Properties();
         try {
             config.load(new FileInputStream("hub.properties"));
@@ -28,10 +30,22 @@ public class ApplicationServiceTest {
             // skip the tests
             assumeTrue(false);
         }
+        service = new ApplicationService(ApplicationService.openDatabase(config), config);
     }
 
     @Test
     public void testOpenDatabase() throws SQLException {
         assertNotNull(ApplicationService.openDatabase(config));
+    }
+
+    @Test
+    public void testGetApplicant() throws SQLException {
+        Applicant applicant = service.getApplicant(55000);
+        assertEquals("Lovelace", applicant.surname);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testGetApplicantInvalidId() throws SQLException {
+        service.getApplicant(9999999);
     }
 }
