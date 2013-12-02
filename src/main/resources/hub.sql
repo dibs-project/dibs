fd532bee02b9ec43bcf95b32a2cfc6767329ab67
Sven Pfaller <sven.pfaller@hu-berlin.de>
2013-12-02 16:30:11 +0100

    hub.sql hinzugefügt

---
+++ src/main/resources/hub.sql
@@ -0,0 +1,7 @@
+CREATE TABLE dosv.settings (
+    semester VARCHAR(6),
+    dosv_users_updated TIMESTAMP,
+    dosv_applications_updated TIMESTAMP
+);
+
+INSERT INTO dosv.settings VALUES ('2014WS', NULL, NULL);
