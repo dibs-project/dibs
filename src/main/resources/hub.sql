337b6c6025dea63df7e4d2068fa63a1591b615e5
Sven Pfaller <sven.pfaller@hu-berlin.de>
2013-12-17 15:55:02 +0100

    Zeitstempel von Settings umbenannt

--- src/main/resources/hub.sql
+++ src/main/resources/hub.sql
@@ -1,7 +1,7 @@
 CREATE TABLE dosv.settings (
     semester VARCHAR(6),
-    dosv_users_updated TIMESTAMP,
-    dosv_applications_updated TIMESTAMP
+    dosv_applicants_update_time TIMESTAMP,
+    dosv_applications_update_time TIMESTAMP
 );
 
 INSERT INTO dosv.settings VALUES ('2014WS', NULL, NULL);
