ddec6a3ab5706a71de616145c11a8eb5b7592c4c
Sven Pfaller <sven.pfaller@hu-berlin.de>
2014-04-29 17:50:27 +0200

    Von Legacy- zu HUB-Datenbank gewechselt

--- src/main/resources/hub.sql
+++ src/main/resources/hub.sql
@@ -1,9 +1,7 @@
--- TODO: Schema zu hub umbenennen
-
-CREATE TABLE dosv.settings (
+CREATE TABLE settings (
     semester VARCHAR(6),
     dosv_applicants_update_time TIMESTAMP,
     dosv_applications_update_time TIMESTAMP
 );
 
-INSERT INTO dosv.settings VALUES ('2014WS', NULL, NULL);
+INSERT INTO settings VALUES ('2014WS', NULL, NULL);
