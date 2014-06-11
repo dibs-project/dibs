9590e54aa9db8ffcab28d7ab877af668934dcf11
Phuong Anh Ha <phuong.anh.ha@hu-berlin.de>
2014-06-11 13:51:04 +0200

    remove documentions for contructor

--- src/main/java/de/huberlin/cms/hub/Course.java
+++ src/main/java/de/huberlin/cms/hub/Course.java
@@ -19,23 +19,13 @@ public class Course {
     private int capacity;
     private ApplicationService service;
 
-    /**
-     * Initialisiert den Studiengang.
-     */
-    public Course(String id, String name, int capacity, ApplicationService service) {
+    Course(String id, String name, int capacity, ApplicationService service) {
         this.id = id;
         this.name = name;
         this.capacity = capacity;
         this.service = service;
     }
 
-    /**
-     * Initialisiert den Studiengang via Datenbankcursor.
-     *
-     * @param results Datenbankcursor, der auf eine Zeile aus <code>course</code> verweist
-     * @param service Bewerbungdienst
-     * @throws SQLException falls ein Datenbankzugriffsfehler auftritt
-     */
     Course(ResultSet results, ApplicationService service) throws SQLException {
         this(results.getString("id"), results.getString("name"),
             results.getInt("capacity"), service);
