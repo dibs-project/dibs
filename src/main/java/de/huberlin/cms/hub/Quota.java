29be094521815aac49b970a14e58f01e9ba1b775
Markus Michler <markus.michler@uv.hu-berlin.de>
2014-06-16 14:34:40 +0200

    Klassen-Stub mit auskommentierten Dependencies

---
+++ src/main/java/de/huberlin/cms/hub/Quota.java
@@ -0,0 +1,38 @@
+package de.huberlin.cms.hub;
+
+/**
+ * @author Markus Michler
+ *
+ */
+public class Quota /* extends HubObject */{
+    private String name;
+    private Double value;
+
+    /* private List<Criterion> rankingCriteria; */
+    /* private List<Criterion> inclusionCriteria; */
+
+    Quota(String id, ApplicationService service, String name, Double value /*,List<Criterion> rankingCriteria, List<Criterion> inclusionCriteria*/) {
+        /* super(id, service); */
+        this.name = name;
+        this.value = value;
+        /* this.rankingCriteria = rankingCriteria; 
+        this.inclusionCriteria = inclusionCriteria;*/
+    }
+
+    //TODO Konstruktor mit Resultset
+
+    /**
+     * Name der Quote
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     */
+    public Double getValue() {
+        return value;
+    }
+
+}
