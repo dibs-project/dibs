e01ec1012eb19683f10a8710f652e76dc97c8167
Markus Michler <markus.michler@uv.hu-berlin.de>
2014-06-18 14:44:45 +0200

    Criterion-Dummy

--- src/main/java/de/huberlin/cms/hub/Quota.java
+++ src/main/java/de/huberlin/cms/hub/Quota.java
@@ -1,25 +1,37 @@
 package de.huberlin.cms.hub;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Markus Michler
  *
  */
 public class Quota extends HubObject {
+    // TODO hub.sql anpassen
     private String name;
     private Double percentage;
+    private List<Criterion> rankingCriteria;
+    private List<Criterion> inclusionCriteria;
 
-    /* private List<Criterion> rankingCriteria; */
-    /* private List<Criterion> inclusionCriteria; */
-
-    Quota(String id, ApplicationService service, String name, Double value /*,List<Criterion> rankingCriteria, List<Criterion> inclusionCriteria*/) {
+    Quota(String id, ApplicationService service, String name, Double percentage,
+        List<Criterion> rankingCriteria, List<Criterion> inclusionCriteria) {
         super(id, service);
         this.name = name;
-        this.percentage = value;
-        /* this.rankingCriteria = rankingCriteria; 
-        this.inclusionCriteria = inclusionCriteria;*/
+        this.percentage = percentage;
+        this.rankingCriteria = rankingCriteria;
+        this.inclusionCriteria = inclusionCriteria;
     }
 
-    //TODO Konstruktor mit Resultset
+    Quota(ResultSet results, ApplicationService service) throws SQLException {
+        // initialisiert den Benutzer über den Datenbankcursor
+        this(results.getString("id"), service, results.getString("name"), results
+            .getDouble("percentage"), new ArrayList<Criterion>(),
+            new ArrayList<Criterion>());
+        // TODO Criterion-Objekte laden
+    }
 
     /**
      * Name der Quote
@@ -29,10 +41,24 @@ public class Quota extends HubObject {
     }
 
     /**
-     *
+     * Prozentualer Anteil der Quote an der Gesamtzahl der vergebenen Studienplätze
      */
     public Double getPercentage() {
         return percentage;
+    }
+
+    /**
+     * Kriterien für die Sortierung auf der Rangliste
+     */
+    public List<Criterion> getRankingCriteria() {
+        return rankingCriteria;
+    }
+
+    /**
+     * Kriterien für die Aufnahme von Bewerbungen in die Quote
+     */
+    public List<Criterion> getInclusionCriteria() {
+        return inclusionCriteria;
     }
 
 }
