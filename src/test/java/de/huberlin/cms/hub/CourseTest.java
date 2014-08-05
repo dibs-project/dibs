f9a22523069b2607b308b034ec38fa35a5b71962
Sven Pfaller <sven.pfaller@hu-berlin.de>
2014-08-05 16:00:53 +0200

    Automatische Zuordnung in Course.apply implementiert

--- src/test/java/de/huberlin/cms/hub/CourseTest.java
+++ src/test/java/de/huberlin/cms/hub/CourseTest.java
@@ -6,13 +6,17 @@
 package de.huberlin.cms.hub;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+
 import org.junit.Test;
-import de.huberlin.cms.hub.AllocationRule;
 
 /**
  * @author Markus Michler
+ * @author Sven Pfaller
  */
 public class CourseTest extends HubTest {
     @Test
@@ -24,7 +28,24 @@ public class CourseTest extends HubTest {
     @Test
     public final void testApply() {
         Application application = course.apply(user.getId(), null);
+        Evaluation evaluation = application.getEvaluationByCriterionId("qualification");
         assertTrue(user.getApplications(null).contains(application));
-        assertTrue(application.getEvaluations(null).size() > 0);
+        assertEquals(Evaluation.STATUS_INFORMATION_MISSING, evaluation.getStatus());
+        assertNull(evaluation.getInformation());
+        assertNull(evaluation.getValue());
+    }
+
+    @Test
+    public void testApplyExistingInformation() {
+        HashMap<String, Object> args = new HashMap<String, Object>();
+        args.put("grade", 4.0);
+        Information information =
+            this.user.createInformation("qualification", args, null);
+
+        Application application = course.apply(user.getId(), null);
+        Evaluation evaluation = application.getEvaluationByCriterionId("qualification");
+        assertEquals(Evaluation.STATUS_EVALUATED, evaluation.getStatus());
+        assertEquals(information, evaluation.getInformation());
+        assertNotNull(evaluation.getValue());
     }
 }
