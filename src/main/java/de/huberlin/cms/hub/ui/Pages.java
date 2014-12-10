c9dfdb46b5590600c410c9f1c3304feebae09438
Sven Pfaller <sven.pfaller@hu-berlin.de>
2014-12-10 17:03:21 +0100

    imporved error handling in course view

--- src/main/java/de/huberlin/cms/hub/ui/Pages.java
+++ src/main/java/de/huberlin/cms/hub/ui/Pages.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.BadRequestException;
 import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -240,13 +239,9 @@ public class Pages implements Closeable {
         Course course = this.service.getCourse(id);
         this.model.put("course", course);
         this.model.put("applications", course.getApplications());
-        if (error != null) {
-            if (error.equals("course_has_applications")) {
-                this.model.put("notification",
-                    "Die Veröffentlichung kann nicht zurückgezogen werden solange es Bewerbungen auf diesen Studiengang gibt.");
-            } else {
-                throw new BadRequestException();
-            }
+        if (error != null && error.equals("course_has_applications")) {
+            this.model.put("notification",
+                "Die Veröffentlichung kann nicht zurückgezogen werden solange es Bewerbungen auf diesen Studiengang gibt.");
         }
         return new Viewable("/course.ftl", this.model);
     }
