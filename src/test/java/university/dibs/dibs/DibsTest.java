cadf9a97ee0de28c92c0eff9d2a8235b71baec2b
Markus Michler <markus.michler@uv.hu-berlin.de>
2015-06-02 15:28:42 +0200

    Linted classes for dosv-soap-message feature

--- src/test/java/university/dibs/dibs/DibsTest.java
+++ src/test/java/university/dibs/dibs/DibsTest.java
@@ -1,20 +1,25 @@
 /*
  * dibs
- * Copyright (C) 2015 Humboldt-Universität zu Berlin
- * 
- * This program is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with this
- * program.  If not, see <http://www.gnu.org/licenses/>
+ * Copyright (C) 2015  Humboldt-Universität zu Berlin
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.  If
+ * not, see <http://www.gnu.org/licenses/>.
  */
 
 package university.dibs.dibs;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -24,26 +29,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
-
-import university.dibs.dibs.ApplicationService;
-import university.dibs.dibs.Course;
-import university.dibs.dibs.User;
-
 /**
  * Basisklasse für dibs-Tests. Übernimmt die Initialisierung (und das anschließende
  * Aufräumen) der Testumgebung. Für Tests werden eine Datenbankverbindung, das
  * Bewerbungssystem und ein allgemeiner Benutzer bereitgestellt.
- * <p>
- * Die Konfiguration der Testumgebung wird aus der Datei <code>test.properties</code>
+ *
+ * <p> Die Konfiguration der Testumgebung wird aus der Datei <code>test.properties</code>
  * gelesen.
  *
  * @author Sven Pfaller
  */
 public abstract class DibsTest {
+    /**
+     * TODO.
+     */
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
@@ -89,9 +88,9 @@ public abstract class DibsTest {
             // ignorieren
         }
 
-        this.db = DriverManager.getConnection(config.getProperty("db_url"),
-            config.getProperty("db_user"),
-            config.getProperty("db_password"));
+        this.db = DriverManager.getConnection(this.config.getProperty("db_url"),
+            this.config.getProperty("db_user"),
+            this.config.getProperty("db_password"));
         ApplicationService.setupStorage(this.db, true);
 
         this.service = new ApplicationService(this.db, this.config);
@@ -100,8 +99,8 @@ public abstract class DibsTest {
             "barber@example.org:secr3t", User.ROLE_APPLICANT);
 
         this.course = this.service.createCourse("Computer Science", 500, false, null);
-        this.course.createAllocationRule(null).createQuota("Standard", 100, null).
-            addRankingCriterion("qualification", null);
+        this.course.createAllocationRule(null).createQuota("Standard", 100, null)
+            .addRankingCriterion("qualification", null);
         this.course.publish(null);
     }
 
